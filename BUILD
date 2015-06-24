3b940c36e20f9101d2311e0296dd74f89d2bc30f
liuhuahang <liuhuahang@zerus.co>
2015-06-24 19:50:34 +0800

    initial project structure
    Change-Id: I28d67de164d41ecdb250af1f56998537f2d1367d
    Signed-off-by: liuhuahang <liuhuahang@zerus.co>

---
+++ BUILD
@@ -0,0 +1,9 @@
+cc_binary(
+  name = 'f2web',
+  srcs = [
+    'F2Web.cpp',
+  ],
+  deps = [
+    '//proxygen:proxygen',
+  ]
+)
---
+++ F2Web.cpp
@@ -0,0 +1,21 @@
+/**
+ * @author Huahang Liu (x@zerus.co)
+ * @date 2014-06-24
+ */
+
+#include <proxygen/httpserver/HTTPServer.h>
+#include <proxygen/httpserver/RequestHandlerFactory.h>
+
+#include <folly/Memory.h>
+#include <folly/Portability.h>
+#include <folly/io/async/EventBaseManager.h>
+
+#include <glog/logging.h>
+
+#include <gflags/gflags.h>
+
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+  return EXIT_SUCCESS;
+}
